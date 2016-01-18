386f3a02adecb983b3b5a799563d5a37c8638a4a
Aleks Kivuls <akivuls@yahoo.com>
2016-01-17 23:04:47 -0800

    Solve HackerRank contest ThirtyDaysOfCode Day17Exceptions
    Implemented logic

---
+++ hackerRank/contests/thirtyDaysOfCode/day17Exceptions/Exceptions.java
@@ -0,0 +1,36 @@
+package hackerRank.contests.thirtyDaysOfCode.day17Exceptions;
+
+import java.util.Scanner;
+
+public class Exceptions {
+
+	public static void main(String[] argh) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		while (T-- > 0) {
+			int n = in.nextInt();
+			int p = in.nextInt();
+			Calculator myCalculator = new Calculator();
+			try {
+				int ans = myCalculator.power(n, p);
+				System.out.println(ans);
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+}
+
+class Calculator {
+	public Calculator() {
+
+	}
+
+	public int power(int n, int p) throws Exception {
+		if (n < 0 || p < 0)
+			throw new Exception("n and p should be non-negative");
+		return (int) Math.pow(n, p);
+	}
+}
