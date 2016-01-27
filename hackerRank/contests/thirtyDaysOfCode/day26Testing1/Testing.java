5c648507d9e07fa2206eac000e62b564b7d185d0
Aleks Kivuls <akivuls@yahoo.com>
2016-01-26 23:50:36 -0800

    Solve HackerRank contest ThirtyDaysOfCode Day26Testing1
    Implemented logic

---
+++ hackerRank/contests/thirtyDaysOfCode/day26Testing1/Testing.java
@@ -0,0 +1,31 @@
+package hackerRank.contests.thirtyDaysOfCode.day26Testing1;
+
+import java.util.Scanner;
+
+public class Testing {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int returnDay = in.nextInt();
+		int returnMo = in.nextInt();
+		int returnYr = in.nextInt();
+
+		int dueDay = in.nextInt();
+		int dueMo = in.nextInt();
+		int dueYr = in.nextInt();
+
+		System.out.println(calculateFine(returnDay, returnMo, returnYr, dueDay, dueMo, dueYr));
+	}
+
+	public static int calculateFine(int returnDay, int returnMo, int returnYr, int dueDay, int dueMo, int dueYr) {
+		if (returnYr > dueYr) {
+			return 10000;
+		}
+		if (returnYr == dueYr && returnMo > dueMo) {
+			return 500 * (returnMo - dueMo);
+		}
+		if (returnYr == dueYr && returnMo == dueMo && returnDay > dueDay) {
+			return 15 * (returnDay - dueDay);
+		}
+		return 0;
+	}
+}
